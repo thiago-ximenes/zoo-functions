b84a418837b3f395be57ce79f9d787e20eda9b0c
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-11 19:28:41 -0300

    Resolve requesito 1 ✅

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  return !ids ? [] : [data.species.find((item) => item.id === ids)];
+  const result = ids.map((id) => {
+    return data.species.find((specie) => specie.id === id);
+  });
+  return !ids ? [] : result;
 }
 
 module.exports = getSpeciesByIds;
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
