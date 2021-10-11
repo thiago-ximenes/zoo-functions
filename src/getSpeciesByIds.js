9752b61be8a001fa31933a242212a8fe6ca9f5d4
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-11 19:16:30 -0300

    Passa no teste 2 de getSpeciesByIds ✅

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
+  return !ids ? [] : [data.species.find((item) => item.id === ids)];
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
