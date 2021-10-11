cc725cd4a1285aa5cfdf2fc7f2942a59a8077501
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-11 19:59:22 -0300

    Resolve requisito 2 ✅

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalNames = data.species.find(
+    (specieName) => specieName.name === animal,
+  );
+  return animalNames.residents.every((animalName) => animalName.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
