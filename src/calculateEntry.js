e874dc5ffb5f2a333c99a8125dbbdee535dff304
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-12 19:00:39 -0300

    Resolve etapa 2 do requisito 6 🧪

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,28 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter(
+    (entrant) => entrant.age >= 18 && entrant.age < 50
+  );
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const people = {
+    adult: adult.length,
+    child: child.length,
+    senior: senior.length,
+  };
+  return people;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// console.log(
+//   countEntrants(),
+// );
