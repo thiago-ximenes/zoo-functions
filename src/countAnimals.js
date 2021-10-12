05f255e44604694e4bf04c002eada2a9d0cbc1ef
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-12 18:28:41 -0300

    Resolve requisito 5 ✅

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,12 +5,21 @@ function countAnimals(animal) {
   if (!animal) {
     const animals = {};
     species.forEach((specie) => {
-      animals[specie.name] = specie.popularity;
+      animals[specie.name] = specie.residents.length;
     });
     return animals;
   }
+  if (animal.sex) {
+    const getAnimal = species.find((specieName) => specieName.name === animal.specie);
+    const lengthPerSex = getAnimal.residents.filter((animalSex) => animal.sex === animalSex.sex);
+    return lengthPerSex.length;
+  }
+  const animalName = species.find(
+    (specie) => specie.name === animal.specie,
+  );
+  return animalName.residents.length;
 }
 
 module.exports = countAnimals;
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
