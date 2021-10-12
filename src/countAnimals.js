c43a8bcde5676913a2a8f6185dc2f1eb429934d2
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-12 16:14:02 -0300

    Resolve etapa 1 do requisito 5 🧪

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
-
+const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const animals = {};
+    species.forEach((specie) => {
+      animals[specie.name] = specie.popularity;
+    });
+    return animals;
+  }
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals());
