7709913b6d83f6fb1ae4ed38a462c1ab316aab03
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-12 10:13:46 -0300

    Resolve Requisito 3 ✅

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,21 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
-  if (!employeeName) return {};
+  const employeeFirstName = data.employees.find(
+    (employee) => employee.firstName === employeeName,
+  );
+  const employeeLastName = data.employees.find(
+    (employee) => employee.lastName === employeeName,
+  );
+  if (employeeFirstName) {
+    return employeeFirstName;
+  }
+  if (employeeLastName) {
+    return employeeLastName;
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName('Burl'));
