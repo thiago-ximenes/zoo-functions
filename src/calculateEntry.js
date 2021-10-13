b15c4c7dfe225a20cd03b2443098faa8da210619
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-13 15:11:58 -0300

    Refatora getRelatedEmployees.js ♻️

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,7 +4,7 @@ function countEntrants(entrants) {
   // seu código aqui
   const child = entrants.filter((entrant) => entrant.age < 18);
   const adult = entrants.filter(
-    (entrant) => entrant.age >= 18 && entrant.age < 50
+    (entrant) => entrant.age >= 18 && entrant.age < 50,
   );
   const senior = entrants.filter((entrant) => entrant.age >= 50);
   const people = {
@@ -20,10 +20,11 @@ function calculateEntry(entrants) {
   if (!entrants || (typeof entrants === 'object' && Object.keys(entrants).length === 0)
   ) return 0;
   // let sumBilling = 0;
-  // if (entrant.age < 18) 
+  // if (entrant.age < 18)
 }
 
 module.exports = { calculateEntry, countEntrants };
 
-//adult = 49.99 senior 24.99 child 20.99
+//  adult = 49.99 senior 24.99 child 20.99
+
 console.log(calculateEntry({}));
