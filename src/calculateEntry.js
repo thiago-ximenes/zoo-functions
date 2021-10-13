846721d35e661542c44da6a9f152834fd5acb9d6
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-13 15:39:49 -0300

    Resolve Requesito 6 ✅

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,7 +4,7 @@ function countEntrants(entrants) {
   // seu código aqui
   const child = entrants.filter((entrant) => entrant.age < 18);
   const adult = entrants.filter(
-    (entrant) => entrant.age >= 18 && entrant.age < 50,
+    (entrant) => entrant.age >= 18 && entrant.age < 50
   );
   const senior = entrants.filter((entrant) => entrant.age >= 50);
   const people = {
@@ -19,12 +19,17 @@ function calculateEntry(entrants) {
   // seu código aqui
   if (!entrants || (typeof entrants === 'object' && Object.keys(entrants).length === 0)
   ) return 0;
-  // let sumBilling = 0;
-  // if (entrant.age < 18)
+  const entrant = countEntrants(entrants);
+  const { adult, child, senior } = entrant;
+
+  const childTicket = 20.99;
+  const adultTicket = 49.99;
+  const seniorTicket = 24.99;
+  const sumBilling =
+    adult * adultTicket + child * childTicket + senior * seniorTicket;
+  return sumBilling;
 }
 
 module.exports = { calculateEntry, countEntrants };
 
 //  adult = 49.99 senior 24.99 child 20.99
-
-console.log(calculateEntry({}));
