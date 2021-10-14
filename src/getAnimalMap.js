d215d32a89ee2edd0e44869e0020d9f642aebf60
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-14 00:02:22 -0300

    Feito com o Douglas e Crys

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const animalMap = species.reduce((accumulator, { location }) => {
+    const animalPerLocation = species.filter((specie) => specie.location === location);
+    const animalName = animalPerLocation.map((specie) => specie.name);
+    const residentsData = animalPerLocation.map((specie) => specie.residents);
+    const residentsName = residentsData.map((resident) =>resident.map((animal) => animal.name));
+    animalName.forEach((name) => {
+      accumulator[location] = { [name]: residentsName };
+    });
+    // accumulator[location] = { [animalName]: residentsName };
+    return accumulator;
+  }, {});
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap());
