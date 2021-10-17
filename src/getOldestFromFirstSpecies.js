831e290108fa3fffa83f273ee32f6db361e28a4f
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-17 16:30:23 -0300

    ✅ Requisito 9.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeById = employees.find((employee) => employee.id === id);
+  const firstSpecie = employeeById.responsibleFor[0];
+  const speciePerEmployee = species.find((specie) => specie.id === firstSpecie);
+  const { residents } = speciePerEmployee;
+  const result = residents.reduce((acc, animal) => {
+    if (animal.age > acc.age) return animal;
+    return acc;
+  });
+  return Object.values(result);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,8 +2,6 @@ const data = require('../data/zoo_data');
 
 const { hours, species } = data;
 
-// const getDayOfTheWeek = (day) => Object.keys(hours).find((hour) => hour === day);
-
 // Get hour when office is open and return in a string
 const getOfficeHour = (day) => {
   const { open, close } = hours[day];
@@ -53,5 +51,3 @@ function getSchedule(scheduleTarget) {
 }
 
 module.exports = getSchedule;
-
-console.log(getSchedule('lions'));
