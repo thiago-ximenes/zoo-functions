45663401200309e13d7256ce871914f18f8632d4
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-17 18:03:13 -0300

    Requisito 10 - etapa 4 🧪.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,13 +2,6 @@ const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
-const getEmployeeByParameter = (employeeParameter) => {
-  const findEmployee = (employee) =>
-    Object.values(employee).some((value) => value === employeeParameter);
-  const result = employees.find(findEmployee);
-  return result;
-};
-
 const getSpeciesOfEmployee = (responsibleFor) => {
   const animals = responsibleFor.map((animal) => species.find((specie) => specie.id === animal));
   const animalsName = animals.map((specie) => specie.name);
@@ -19,22 +12,32 @@ const getSpeciesOfEmployee = (responsibleFor) => {
   };
 };
 
+const getAllEmployeesId = () => {
+  const result = employees.reduce((acc, { id, firstName, lastName, responsibleFor }) => {
+    const speciesOfEmployee = getSpeciesOfEmployee(responsibleFor);
+    const { animalsName, animalsLocation } = speciesOfEmployee;
+    acc.push({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: animalsName,
+      locations: animalsLocation,
+    });
+    return acc;
+  }, []);
+  return result;
+};
+
 function getEmployeesCoverage(employeeParameter) {
   // seu código aqui
-  const employee = Object.values(employeeParameter);
-  const employeeObject = getEmployeeByParameter(employee[0]);
-  const { id, firstName, lastName, responsibleFor } = employeeObject;
-  const speciesOfEmployee = getSpeciesOfEmployee(responsibleFor);
-  const { animalsName, animalsLocation } = speciesOfEmployee;
-  const result = {
-    id,
-    fullName: `${firstName} ${lastName}`,
-    species: animalsName,
-    locations: animalsLocation,
-  };
+  if (!employeeParameter) return getAllEmployeesId();
+  const employeeCoverage = Object.values(employeeParameter)[0];
+  const allEmployees = getAllEmployeesId();
+  const result = allEmployees.find((employee) => {
+    const fullNameSplitted = employee.fullName.split(' ');
+    const someFunction = (value) => value === employeeCoverage;
+    return employee.id === employeeCoverage || fullNameSplitted.some(someFunction);
+  });
   return result;
 }
 
 module.exports = getEmployeesCoverage;
-
-console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
